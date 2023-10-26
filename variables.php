763918b0681e6e6960220c3c2a7f1c46a36254ec
Juan Mora <moramamba@gmail.com>
2023-10-26 15:52:49 -0600

    first commit

---
+++ variables.php
@@ -0,0 +1,21 @@
+<?php
+
+# String: cadena de texto
+$nombre = "juam mora";
+
+# Integer: Numeros enteros
+$numero = 7;
+
+# Double: Numeros con decimales
+$numero_decimasl = 7.7;
+
+# Boleano: Verdadero o falso (true / false)
+$verdadero = fals;
+
+# Array: Arreglo
+# Objeto
+# Nuell: Cuando a un avariable no se la ha asignado ningun valor
+
+echo 'Hola, ' . $nombre;
+
+?>
