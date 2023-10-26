9be062d3be94b328642c66360720de04580c8683
Juan Mora <moramamba@gmail.com>
2023-10-26 16:37:46 -0600

    quitando parte del textodesde master

--- hola_mundo.php
+++ hola_mundo.php
@@ -12,7 +12,7 @@
         <title>Document</title>
     </head>
     <body>
-        <h1><?php echo "Hola mundo horrible"; ?> </h1>
+        <h1><?php echo "Hola mundo "; ?> </h1>
     </body>
 </html>
 
