f7a19840595a91a10d0e53d669f1205d1b2fa798
Juan Mora <moramamba@gmail.com>
2023-11-03 13:32:24 -0600

    agregando archivos nuevos

---
+++ areglo _asociativo.php
@@ -0,0 +1,11 @@
+<?php
+
+$alex = array('telefono' => '7773152073','edad' => 40 ,'apellido' => 'falconmaster', 'pais' =>'mexico');
+
+echo $alex['telefono'] . '<br/>';
+echo $alex['edad'] . '<br/>';
+echo $alex['apellido'] . '<br/>';
+echo $alex['pais'] . '<br/>';
+
+
+?>
---
+++ arreglos_multidimensionales.php
@@ -0,0 +1,13 @@
+<?php
+
+$amigos =array( 
+    array('alejandro' ,20), 
+    array('carlos' , 30), 
+    array('manuel', 30)
+);
+
+echo $amigos[0][0] . '<br />';
+echo $amigos[1][0] . '<br />';
+echo $amigos[2][0] . '<br />';
+
+?>
