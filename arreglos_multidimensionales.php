9fe83dbd736560b4661e3cbbd5235a4095129fc4
Juan Mora <moramamba@gmail.com>
2023-11-03 13:59:30 -0600

    contando los elementos de un arreglo

--- arreglos_multidimensionales.php
+++ arreglos_multidimensionales.php
@@ -10,4 +10,18 @@ echo $amigos[0][0] . '<br />';
 echo $amigos[1][0] . '<br />';
 echo $amigos[2][0] . '<br />';
 
+// conociendo el numero de elementos de un arreglo
+
+$meses =array(
+    'Enero', 'Febrero', 'Marzo', 'Abril', 
+    'Mayo', 'Junio', 'Julio', 'Agosto', 
+    'Septiembre','Octubre', 'Noviembre', 'Diciembre'
+);
+
+// este comando nos sive para contar los elementos del arreglo
+echo count($meses);
+
+// a continuacion crearemos una variable llamada $ultimo_mes que nos dara el nombre del ultimo mes de arreglo
+$ultimo_mes = count($meses) -1;
+
 ?>
