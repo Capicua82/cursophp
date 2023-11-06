48fa3d3f54f08e648fb9a11bca74533c27abac72
Juan Mora <moramamba@gmail.com>
2023-11-06 12:10:12 -0600

    modificando archivos

--- condicionales.php
+++ condicionales.php
@@ -4,14 +4,35 @@
 // instrucciones
 //}
 
-$edad = 18;
 
-if ($edad >=18); {
+/* Operadores de comparación.
+
+== (igual que)
+< (menor que)
+> (mayor que)
+<= (menor o igual que)
+>= (mayor o igual que)
+!= (diferente que)
+!  (negación)
+*/
+
+/* operadores lógicos
+ 
+&& evalua que se cumplan las dos condiciones
+|| OR  Evalua que se cumpla al menos una de las dos condiciones
+xor Evalua quese cumpla una y solo una condición
+
+*/
+
+$edad = 18;
+$nombre = 'carlos';
+
+if ($edad >=18 && $nombra == 'carlos'); {
     echo '<h1> Bienvenidp </h1>';
     
-
-if ($edad <18); {
-        echo '<h1> eres menor de edad </h1>';
+}
+if ($edad <18 or $nombre != 'carlos'); {
+        echo '<h1> eres menor de edad y/o no te llamas carlos </h1>';
 }
     
 
