cf42f0ae1f4201ebc8b811f58d2c7e062e2f5ed1
Juan Mora <moramamba@gmail.com>
2023-11-09 18:01:24 -0600

    agregando archivos

---
+++ operadores.php
@@ -0,0 +1,108 @@
+<?php
+/*  Operadores Aritméticos:
+
++ (suma)
+- (resta)
+* (multiplicación)
+/ (división)
+*/
+
+// $numero = 10;
+// $numero2 = 5;
+
+// $reaultado = $numero + $numero2;
+// echo $resultado;
+
+/*
+
+Operadores de Asignación:
+
+= (asignación)
++= (suma y asignación)
+-= (resta y asignación)
+*= (multiplicación y asignación)
+/= (división y asignación)
+*/
+
+// $numero = 10;
+// $numero2 = 5;
+
+// $numero = $numero + 7;
+// $numero += 7;
+// echo $numero;
+
+// operadores de comparacion
+
+/*
+== 
+=== 
+!= o <>
+!== 
+< 
+> 
+<=
+>= 
+
+/*
+ $numero = '10'; // string
+$variable = true; // boleano
+
+ if $numero($numero === 10) {
+    echo 'Es identico';
+ }  else { 
+    echo 'Es diferente'; 
+     
+ }
+*/
+
+/*
+operadores logicos
+
+&& o and (y)
+|| o or (o)
+! 
+
+$numero 0 10;
+
+if ($numero >= 10 && $numero > 20){
+    echo 'Ejecutar';
+}
+*/
+
+/*
+
+operadores de incremento y decremento
+
+++ $x
+$x **
+-- $x
+$x --
+--
+
+$numero = 10;
+$numero = $numero +1;
+$numero +=  1;
+
+$numero ++;
+echo $numero--;
+echo "<br />";
+echo $numero;
+*/
+
+/*
+operadores de cadena (concatenar)
+
+.
+.=
+--
+
+$texto 'cadena de texto';
+$texto2 ' una segunfda cadena de texto';
+
+$texto3 = $texto . $texto2;
+echo $texto3;
+*/
+
+
+
+?>
